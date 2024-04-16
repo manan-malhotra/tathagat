f2d38921473922a235be31ce8f1f372bba8d8c72
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-04-16 23:25:12 +0530

    Twil Additions

--- index.js
+++ index.js
@@ -12,10 +12,40 @@ app.get("/", (req, res) => {
 const accountSidOTP = process.env.ACCOUNT_SID_OTP;
 const authTokenOTP = process.env.AUTH_TOKEN_OTP;
 const serviceSidOTP = process.env.SERVICE_SID;
+app.post("/send-otp", async (req, res) => {
+    const { phoneNumber } = req.body;
+    console.log("reached");
+    try {
+        const verification = await client.verify
+            .services(serviceSidOTP)
+            .verifications.create({ to: phoneNumber, channel: "sms" });
+        console.log(`OTP sent to ${phoneNumber}: ${verification.sid}`);
+        res.status(200).json({ message: "OTP sent successfully" });
+    } catch (error) {
+        console.error("Error sending OTP:", error);
+        res.status(500).json({ error: "Failed to send OTP" });
+    }
+});
 
-// app.listen(port, () => {
-//     console.log(`Server is listening on port ${port}`);
-// });
+app.post("/verify-otp", async (req, res) => {
+    const { phoneNumber, code } = req.body;
+
+    try {
+        const verificationCheck = await client.verify
+            .services(serviceSidOTP)
+            .verificationChecks.create({ to: phoneNumber, code: code });
+        if (verificationCheck.status === "approved") {
+            console.log("OTP verified successfully");
+            res.status(200).json({ message: "OTP verified successfully" });
+        } else {
+            console.log("Invalid OTP");
+            res.status(400).json({ error: "Invalid OTP" });
+        }
+    } catch (error) {
+        console.error("Error verifying OTP:", error);
+        res.status(500).json({ error: "Failed to verify OTP" });
+    }
+});
 
 const server = () => {
     app.listen(port, "0.0.0.0", () => {
