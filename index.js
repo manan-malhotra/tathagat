9f7b0d6920856e192a6564e2cb75f6557ac0d5bb
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-04-16 23:30:01 +0530

    Twil Client

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
+const twilio = require("twilio");
 app.use(express.json());
 app.use(cors());
 app.get("/", (req, res) => {
