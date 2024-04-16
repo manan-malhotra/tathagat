8b0063a293d1f4983d1195a916c4ffc585e5a38d
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-04-16 23:14:49 +0530

    Twil

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const twilio = require("twilio");
 const dotenv = require("dotenv");
 const cors = require("cors");
 dotenv.config();
@@ -16,43 +15,6 @@ const serviceSidOTP = process.env.SERVICE_SID;
 app.get("/", (req, res) => {
     res.status(200).json({ message: "Hi" });
 });
-app.post("/send-otp", async (req, res) => {
-    const client = twilio(accountSidOTP, authTokenOTP);
-    const { phoneNumber } = req.body;
-    console.log("reached");
-    try {
-        const verification = await client.verify
-            .services(serviceSidOTP)
-            .verifications.create({ to: phoneNumber, channel: "sms" });
-        console.log(`OTP sent to ${phoneNumber}: ${verification.sid}`);
-        res.status(200).json({ message: "OTP sent successfully" });
-    } catch (error) {
-        console.error("Error sending OTP:", error);
-        res.status(500).json({ error: "Failed to send OTP" });
-    }
-});
-
-app.post("/verify-otp", async (req, res) => {
-    const client = twilio(accountSidOTP, authTokenOTP);
-    const { phoneNumber, code } = req.body;
-
-    try {
-        const verificationCheck = await client.verify
-            .services(serviceSidOTP)
-            .verificationChecks.create({ to: phoneNumber, code: code });
-        if (verificationCheck.status === "approved") {
-            console.log("OTP verified successfully");
-            res.status(200).json({ message: "OTP verified successfully" });
-        } else {
-            console.log("Invalid OTP");
-            res.status(400).json({ error: "Invalid OTP" });
-        }
-    } catch (error) {
-        console.error("Error verifying OTP:", error);
-        res.status(500).json({ error: "Failed to verify OTP" });
-    }
-});
-
 // app.listen(port, () => {
 //     console.log(`Server is listening on port ${port}`);
 // });
@@ -61,6 +23,7 @@ const server = () => {
     app.listen(port, () => {
         console.log(`Server running on port ${port}`);
     });
+    console.log(serviceSidOTP);
     console.log("Testing app running on port 3001");
 };
 
